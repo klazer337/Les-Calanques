15c8249ee54fec73c78ba5e2a063b5079ce411f5
Kevin Trebossen <kevintrebossen@imac-de-kevin.local>
2018-09-26 00:03:38 +0200

    Commit logique UITableView Delegate

--- Les Calanques/Controller/ControllerAvecTableView.swift
+++ Les Calanques/Controller/ControllerAvecTableView.swift
@@ -8,23 +8,38 @@
 
 import UIKit
 
-class ControllerAvecTableView: UIViewController {
+class ControllerAvecTableView: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    var calanques: [Calanque] = []
+    var cellID = "Alternative"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        tableView.delegate = self
+        tableView.dataSource = self
+        calanques = CalanqueCollection().all()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return calanques.count
     }
-    */
-
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let calanque = calanques[indexPath.row]     // création de la calanque selon l'indexpath
+        if let cell = tableView.dequeueReusableCell(withIdentifier: cellID) as? CalanqueCellAlternative {
+            cell.calanque = calanque
+            return cell
+        }
+        return UITableViewCell()
+    }
+    
+    // Hauteur de la row
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 200
+    }
+    
+    
 }
